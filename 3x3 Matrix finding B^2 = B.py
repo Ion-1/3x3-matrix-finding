becff22c7123ab813fce81fddc4ac1da74352bde
skalameraion <32754510+skalameraion@users.noreply.github.com>
2019-08-29 21:04:49 +0800

    Added the programm 3x3 Matrix finding B^2 = B
    Python 3 Programm
    Find 3x3 Matrix
    B^2  = B
    a1,b1,c1
    a2,b2,c2
    a3,b3,c3

---
+++ 3x3 Matrix finding B^2 = B.py
@@ -0,0 +1,486 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 20:22:48 2019
+
+@author: Admin
+"""
+
+a1 = -1
+a2 = 0
+a3 = 0
+b1 = 0
+b2 = 0
+b3 = 0
+c1 = 0
+c2 = 0
+c3 = 0
+
+x = 0
+if x == 0:
+    for i in range(10):
+        a1 += 1
+        if a1**2 + b1*a2 + c1*a3 == a1:
+            if a1*a2 + b2*a2 + c2*a3 == a2:
+                if a1*a3 + b3*a2 + c3*a3 == a3:
+                    if a1*b1 + b1*b2 + c1*b3 == b1:
+                        if b1*a2 + b2**2 + c2*b3 == b2:
+                            if b1*a3 + b3*b2 + c3*b3 == b3:
+                                if c1*a1 + b1*c2 + c1*c3 == c1:
+                                    if c1*a2 + b2*c2 + c2*c3 == c2:
+                                        if c1*a3 + b3*c2 + c3**2 == c3:
+                                            print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+    x+=1
+a1 = -1
+a2 = -1
+a3 = 0
+b1 = 0
+b2 = 0
+b3 = 0
+c1 = 0
+c2 = 0
+c3 = 0
+if x == 1:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            if a1**2 + b1*a2 + c1*a3 == a1:
+                if a1*a2 + b2*a2 + c2*a3 == a2:
+                    if a1*a3 + b3*a2 + c3*a3 == a3:
+                        if a1*b1 + b1*b2 + c1*b3 == b1:
+                            if b1*a2 + b2**2 + c2*b3 == b2:
+                                if b1*a3 + b3*b2 + c3*b3 == b3:
+                                    if c1*a1 + b1*c2 + c1*c3 == c1:
+                                        if c1*a2 + b2*c2 + c2*c3 == c2:
+                                            if c1*a3 + b3*c2 + c3**2 == c3:
+                                                print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            else:
+                pass
+        a2 = -1
+    x+=1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = 0
+b2 = 0
+b3 = 0
+c1 = 0
+c2 = 0
+c3 = 0
+if x == 2:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                if a1**2 + b1*a2 + c1*a3 == a1:
+                    if a1*a2 + b2*a2 + c2*a3 == a2:
+                        if a1*a3 + b3*a2 + c3*a3 == a3:
+                            if a1*b1 + b1*b2 + c1*b3 == b1:
+                                if b1*a2 + b2**2 + c2*b3 == b2:
+                                    if b1*a3 + b3*b2 + c3*b3 == b3:
+                                        if c1*a1 + b1*c2 + c1*c3 == c1:
+                                            if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                if c1*a3 + b3*c2 + c3**2 == c3:
+                                                    print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                else:
+                                                    pass
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            a3 = -1
+        a2 = -1
+    x+=1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = -1
+b2 = 0
+b3 = 0
+c1 = 0
+c2 = 0
+c3 = 0
+if x == 3:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                for l in range(10):
+                    b1 += 1
+                    if a1**2 + b1*a2 + c1*a3 == a1:
+                        if a1*a2 + b2*a2 + c2*a3 == a2:
+                            if a1*a3 + b3*a2 + c3*a3 == a3:
+                                if a1*b1 + b1*b2 + c1*b3 == b1:
+                                    if b1*a2 + b2**2 + c2*b3 == b2:
+                                        if b1*a3 + b3*b2 + c3*b3 == b3:
+                                            if c1*a1 + b1*c2 + c1*c3 == c1:
+                                                if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                    if c1*a3 + b3*c2 + c3**2 == c3:
+                                                        print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                    else:
+                                                        pass
+                                                else:
+                                                    pass
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                b1 = -1
+            a3 = -1
+        a2 = -1
+    x+=1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = -1
+b2 = -1
+b3 = 0
+c1 = 0
+c2 = 0
+c3 = 0
+if x == 4:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                for l in range(10):
+                    b1 += 1
+                    for m in range(10):
+                        b2 += 1
+                        if a1**2 + b1*a2 + c1*a3 == a1:
+                            if a1*a2 + b2*a2 + c2*a3 == a2:
+                                if a1*a3 + b3*a2 + c3*a3 == a3:
+                                    if a1*b1 + b1*b2 + c1*b3 == b1:
+                                        if b1*a2 + b2**2 + c2*b3 == b2:
+                                            if b1*a3 + b3*b2 + c3*b3 == b3:
+                                                if c1*a1 + b1*c2 + c1*c3 == c1:
+                                                    if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                        if c1*a3 + b3*c2 + c3**2 == c3:
+                                                            print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                        else:
+                                                            pass
+                                                    else:
+                                                        pass
+                                                else:
+                                                    pass
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                    b2 = -1
+                b1 = -1
+            a3 = -1
+        a2 = -1
+    x+=1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = -1
+b2 = -1
+b3 = -1
+c1 = 0
+c2 = 0
+c3 = 0
+if x == 5:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                for l in range(10):
+                    b1 += 1
+                    for m in range(10):
+                        b2 += 1
+                        for n in range(10):
+                            b3 += 1
+                            if a1**2 + b1*a2 + c1*a3 == a1:
+                                if a1*a2 + b2*a2 + c2*a3 == a2:
+                                    if a1*a3 + b3*a2 + c3*a3 == a3:
+                                        if a1*b1 + b1*b2 + c1*b3 == b1:
+                                            if b1*a2 + b2**2 + c2*b3 == b2:
+                                                if b1*a3 + b3*b2 + c3*b3 == b3:
+                                                    if c1*a1 + b1*c2 + c1*c3 == c1:
+                                                        if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                            if c1*a3 + b3*c2 + c3**2 == c3:
+                                                                print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                            else:
+                                                                pass
+                                                        else:
+                                                            pass
+                                                    else:
+                                                        pass
+                                                else:
+                                                    pass
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            else:
+                                pass
+                        b3 = -1
+                    b2 = -1
+                b1 = -1
+            a3 = -1
+        a2 = -1
+    x += 1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = -1
+b2 = -1
+b3 = -1
+c1 = -1
+c2 = 0
+c3 = 0
+if x == 6:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                for l in range(10):
+                    b1 += 1
+                    for m in range(10):
+                        b2 += 1
+                        for n in range(10):
+                            b3 += 1
+                            for o in range(10):
+                                c1 += 1
+                                if a1**2 + b1*a2 + c1*a3 == a1:
+                                    if a1*a2 + b2*a2 + c2*a3 == a2:
+                                        if a1*a3 + b3*a2 + c3*a3 == a3:
+                                            if a1*b1 + b1*b2 + c1*b3 == b1:
+                                                if b1*a2 + b2**2 + c2*b3 == b2:
+                                                    if b1*a3 + b3*b2 + c3*b3 == b3:
+                                                        if c1*a1 + b1*c2 + c1*c3 == c1:
+                                                            if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                                if c1*a3 + b3*c2 + c3**2 == c3:
+                                                                    print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                                else:
+                                                                    pass
+                                                            else:
+                                                                pass
+                                                        else:
+                                                            pass
+                                                    else:
+                                                        pass
+                                                else:
+                                                    pass
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            c1 = -1
+                        b3 = -1
+                    b2 = -1
+                b1 = -1
+            a3 = -1
+        a2 = -1
+    x += 1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = -1
+b2 = -1
+b3 = -1
+c1 = -1
+c2 = -1
+c3 = 0
+if x == 7:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                for l in range(10):
+                    b1 += 1
+                    for m in range(10):
+                        b2 += 1
+                        for n in range(10):
+                            b3 += 1
+                            for o in range(10):
+                                c1 += 1
+                                for p in range(10):
+                                    c2 += 1
+                                    if a1**2 + b1*a2 + c1*a3 == a1:
+                                        if a1*a2 + b2*a2 + c2*a3 == a2:
+                                            if a1*a3 + b3*a2 + c3*a3 == a3:
+                                                if a1*b1 + b1*b2 + c1*b3 == b1:
+                                                    if b1*a2 + b2**2 + c2*b3 == b2:
+                                                        if b1*a3 + b3*b2 + c3*b3 == b3:
+                                                            if c1*a1 + b1*c2 + c1*c3 == c1:
+                                                                if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                                    if c1*a3 + b3*c2 + c3**2 == c3:
+                                                                        print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                                    else:
+                                                                        pass
+                                                                else:
+                                                                    pass
+                                                            else:
+                                                                pass
+                                                        else:
+                                                            pass
+                                                    else:
+                                                        pass
+                                                else:
+                                                    pass
+                                            else:
+                                                pass
+                                        else:
+                                            pass
+                                    else:
+                                        pass
+                                c2 = -1
+                            c1 = -1
+                        b3 = -1
+                    b2 = -1
+                b1 = -1
+            a3 = -1
+        a2 = -1
+    x += 1
+a1 = -1
+a2 = -1
+a3 = -1
+b1 = -1
+b2 = -1
+b3 = -1
+c1 = -1
+c2 = -1
+c3 = -1
+if x == 8:
+    for i in range(10):
+        a1 += 1
+        for j in range(10):
+            a2 += 1
+            for k in range(10):
+                a3 += 1
+                for l in range(10):
+                    b1 += 1
+                    for m in range(10):
+                        b2 += 1
+                        for n in range(10):
+                            b3 += 1
+                            for o in range(10):
+                                c1 += 1
+                                for p in range(10):
+                                    c2 += 1
+                                    for q in range(10):
+                                        c3 += 1
+                                        if a1**2 + b1*a2 + c1*a3 == a1:
+                                            if a1*a2 + b2*a2 + c2*a3 == a2:
+                                                if a1*a3 + b3*a2 + c3*a3 == a3:
+                                                    if a1*b1 + b1*b2 + c1*b3 == b1:
+                                                        if b1*a2 + b2**2 + c2*b3 == b2:
+                                                            if b1*a3 + b3*b2 + c3*b3 == b3:
+                                                                if c1*a1 + b1*c2 + c1*c3 == c1:
+                                                                    if c1*a2 + b2*c2 + c2*c3 == c2:
+                                                                        if c1*a3 + b3*c2 + c3**2 == c3:
+                                                                            print(a1,a2,a3,b1,b2,b3,c1,c2,c3)
+                                                                        else:
+                                                                            break
+                                                                    else:
+                                                                        break
+                                                                else:
+                                                                    break
+                                                            else:
+                                                                break
+                                                        else:
+                                                            break
+                                                    else:
+                                                        break
+                                                else:
+                                                    break
+                                            else:
+                                                break
+                                        else:
+                                            break
+                                    c3 = -1
+                                c2 = -1
+                            c1 = -1
+                        b3 = -1
+                    b2 = -1
+                b1 = -1
+            a3 = -1
+        a2 = -1
+    x += 1
